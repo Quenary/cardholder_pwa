d6a4f63a9b527904c4edf5c9075cf7e29509d75d
Eugene Savin <evgeny.savin.nord@gmail.com>
2025-06-18 15:09:07 +0300

    + add networks to docker-compose + fix healthcheck for postgres db + add asyncpg to requirements + add postgres url sync/async replace in syssion.py + change arguments of engine/session in session.py

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -10,13 +10,37 @@ def _get_db_url(is_async: bool) -> str:
     url = Config.DB_URL
     if is_async:
         url = url.replace("sqlite:", "sqlite+aiosqlite:")
+        url = url.replace("postgresql+psycopg2:", "postgresql+asyncpg:")
     else:
         url = url.replace("sqlite+aiosqlite:", "sqlite:")
+        url = url.replace("postgresql+asyncpg:", "postgresql+psycopg2:")
     return url
 
 
-engine = create_engine(_get_db_url(False), connect_args={"check_same_thread": False})
-_session_maker = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+def _get_connect_args(
+    url: str,
+) -> dict:
+    if "sqlite:" in url:
+        return {"check_same_thread": False, "timeout": 15}
+    elif "sqlite+aiosqlite:" in url:
+        return {"timeout": 15}
+    return {}
+
+
+sync_url = _get_db_url(False)
+engine = engine = create_engine(
+    sync_url,
+    connect_args=_get_connect_args(sync_url),
+    pool_pre_ping=True,
+    pool_recycle=1800,
+    echo=False,
+)
+_session_maker = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+    expire_on_commit=False,
+)
 
 
 def get_db() -> Generator[Session, None, None]:
@@ -27,9 +51,16 @@ def get_db() -> Generator[Session, None, None]:
         db.close()
 
 
-async_engine = create_async_engine(_get_db_url(True), echo=False)
+async_url = _get_db_url(True)
+async_engine = async_engine = create_async_engine(
+    async_url,
+    connect_args=_get_connect_args(async_url),
+    pool_pre_ping=True,
+    pool_recycle=1800,
+    echo=False,
+)
 _async_session_maker = async_sessionmaker(
-    async_engine,
+    bind=async_engine,
     class_=AsyncSession,
     expire_on_commit=False,
 )
