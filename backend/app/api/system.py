b1bca6ae30e34125187b37dd383d61254cb82608
Eugene Savin <evgeny.savin.nord@gmail.com>
2025-06-18 13:40:33 +0300

    Postgres support + add docker-compose.pg.yml + add docker-compose.sqlite.yml + remove docker-compose.prod.yml + add healthcheck endpoint /system/health + add healthchecks to docker-compose + add pg driver to requirements.txt

--- backend/app/api/system.py
+++ backend/app/api/system.py
@@ -1,16 +1,19 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from app.schemas import Version
 import tomllib
 import os
 from app.core.smtp import EmailSender
 import app.core.auth as auth
+from app.db import get_async_db
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.db.models import User
 
 router = APIRouter(tags=["system"], prefix="/system")
 
 
-# rename to system, add token verification, add smtp status request
 @router.get("/version", response_model=Version)
-def get_version():
+def get_version(_: User = Depends(auth.get_current_user)):
     image_version = os.getenv("VERSION") or None
     toml_path = os.path.join(os.path.dirname(__file__), "..", "..", "pyproject.toml")
     with open(toml_path, "rb") as f:
@@ -20,14 +23,23 @@ def get_version():
 
 
 @router.get("/smtp/status", response_model=bool)
-def get_smtp_status():
+def get_smtp_status(_: User = Depends(auth.get_current_user)):
     return EmailSender.status()
 
 
 @router.post("/smtp/test")
-def send_test_email(current_user=Depends(auth.get_current_user)):
+def send_test_email(current_user: User = Depends(auth.get_current_user)):
     EmailSender.send_email(
         current_user.email,
         "Test email from Cardholder PWA",
         "This is body of the test email.\nGood day!\n",
     )
+
+
+@router.get("/health")
+async def health(db: AsyncSession = Depends(get_async_db)):
+    try:
+        await db.execute(text("SELECT 1"))
+        return "OK"
+    except Exception:
+        raise HTTPException(503, "db_error")
--- backend/start.py
+++ backend/start.py
@@ -13,4 +13,4 @@ def run_migrations():
 
 if __name__ == "__main__":
     run_migrations()
-    uvicorn.run("app.app:app", host="127.0.0.1", port=8000)
+    uvicorn.run("app.app:app", host="localhost", port=8000)
