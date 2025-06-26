7485b11da944f77c6395007099d32ec0987198d6
Eugene Savin <evgeny.savin.nord@gmail.com>
2025-06-26 18:20:29 +0300

    refactor(frontend): add base api service class
    + base api class added
    + all api services now extends BaseApiService

--- frontend/src/app/entities/admin/admin-api.service.ts
+++ frontend/src/app/entities/admin/admin-api.service.ts
@@ -1,52 +1,49 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { EUserRole, IUser } from '../user/user-interface';
-import { environment } from '@env/environment';
 import { ISetting, ISettingUpdate } from './admin-interface';
+import { BaseApiService } from '../base/base-api.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AdminApiService {
-  private readonly httpClient = inject(HttpClient);
+export class AdminApiService extends BaseApiService<'admin'> {
+  protected override readonly prefix = 'admin';
 
   usersList(): Observable<IUser[]> {
-    return this.httpClient.get<IUser[]>(`${environment.api}/admin/users`);
+    return this.httpClient.get<IUser[]>(`${this.basePath}/users`);
   }
 
   deleteUser(user_id: number): Observable<any> {
-    return this.httpClient.delete(`${environment.api}/admin/users/${user_id}`);
+    return this.httpClient.delete(`${this.basePath}/users/${user_id}`);
   }
 
   changeUserRole(user_id: number, role_code: EUserRole): Observable<any> {
     return this.httpClient.put(
-      `${environment.api}/admin/users/role`,
+      `${this.basePath}/users/role`,
       {},
       {
         params: {
           user_id,
           role_code,
         },
-      }
+      },
     );
   }
 
   getSettings(): Observable<ISetting[]> {
-    return this.httpClient.get<ISetting[]>(`${environment.api}/admin/settings`);
+    return this.httpClient.get<ISetting[]>(`${this.basePath}/settings`);
   }
 
   setSettings(settings: ISettingUpdate[]): Observable<any> {
-    return this.httpClient.patch(`${environment.api}/admin/settings`, settings);
+    return this.httpClient.patch(`${this.basePath}/settings`, settings);
   }
 
   smtpStatus(): Observable<boolean> {
-    return this.httpClient.get<boolean>(
-      `${environment.api}/admin/smtp/status`
-    );
+    return this.httpClient.get<boolean>(`${this.basePath}/smtp/status`);
   }
 
   smtpTest(): Observable<any> {
-    return this.httpClient.post(`${environment.api}/admin/smtp/test`, {});
+    return this.httpClient.post(`${this.basePath}/smtp/test`, {});
   }
 }
--- frontend/src/app/entities/public/public-api.service.ts
+++ frontend/src/app/entities/public/public-api.service.ts
@@ -1,20 +1,19 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IVersion } from './public-interface';
-import { environment } from '@env/environment';
+import { BaseApiService } from '../base/base-api.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class PublicApiService {
-  constructor(private readonly httpClient: HttpClient) {}
+export class PublicApiService extends BaseApiService<'public'> {
+  protected override readonly prefix = 'public';
 
   version(): Observable<IVersion> {
-    return this.httpClient.get<IVersion>(`${environment.api}/public/version`);
+    return this.httpClient.get<IVersion>(`${this.basePath}/version`);
   }
 
   health(): Observable<any> {
-    return this.httpClient.get(`${environment.api}/public/health`);
+    return this.httpClient.get(`${this.basePath}/health`);
   }
 }
